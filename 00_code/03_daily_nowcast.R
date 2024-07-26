97b4ee64abc0a8083a5eccb1552a4f9a15ea1da0
Yannic Prohaska <yannicprohaska@gmail.com>
2024-07-27 00:09:37 +0200

    clean_code

--- 00_code/03_daily_nowcast.R
+++ 00_code/03_daily_nowcast.R
@@ -255,25 +255,9 @@ for (j in 1:length(nowcast_results)) {
   }
 }
 
-# ## calculate mse for each doq and separately for different nowcast numbers
-# 
-# nowcast_results_dt[,mse_pmi_bench:= mean(se_pmi_bench), by = .(doq, nowcast_number)]
-# nowcast_results_dt[,mse_news:= mean(se_news), by = .(doq, nowcast_number)]
-# 
-# nowcast_mse <- unique(nowcast_results_dt[,.(doq, nowcast_number, mse_pmi_bench, mse_news)])
-# nowcast_mse[, mse_reduction:= mse_pmi_bench - mse_news]
-# 
-# nowcast_mse_next_release <- nowcast_mse[nowcast_number %in% 1, ]
-# nowcast_mse_nextnext_release <- nowcast_mse[nowcast_number %in% 2, ]
-# 
-# ## calculate mse for each doq
-# 
-# nowcast_results_dt[,mse_pmi_bench:= mean(se_pmi_bench), by = .(doq)]
-# nowcast_results_dt[,mse_news:= mean(se_news), by = .(doq)]
-# nowcast_mse <- unique(nowcast_results_dt[,.(doq, nowcast_number, mse_pmi_bench, mse_news)])
-# nowcast_mse[, mse_reduction:= mse_pmi_bench - mse_news]
 
 ## only real "nowcasts" --> quarter of date of nowcast = quarter for which nowcast is for
+# --> here we only have "real" nowcasts anyway
 
 nowcast_results_dt[, quarter_of_nc:= floor_date(as.Date(day), unit = "quarter")]
 nowcast_results_dt_rn <- nowcast_results_dt[quarter == quarter_of_nc,]
@@ -357,22 +341,6 @@ nowcast_results_dt_rn <- nowcast_results_dt[quarter == quarter_of_nc,]
 }
 
 
-## simple plot
-
-# plot(nowcast_mse_next_release$doq, nowcast_mse_next_release$mse_reduction, type = "l")
-# plot(nowcast_mse_nextnext_release$doq, nowcast_mse_nextnext_release$mse_reduction, type = "p")
-# 
-# 
-# plot(nowcast_mse$doq, nowcast_mse$mse_reduction)
-# plot(nowcast_mse$doq, nowcast_mse$mse_pmi_bench, type = "p")
-# plot(nowcast_mse$doq, nowcast_mse$mse_news)
-# 
-# plot(nowcast_rn_mse$doq, nowcast_rn_mse$mse_pmi_bench)
-# plot(nowcast_rn_mse$doq, nowcast_rn_mse$mse_news)
-# plot(nowcast_rn_mse$doq, nowcast_rn_mse$mse_news_cci)
-# 
-# ggplot(data = nowcast_rn_mse, aes())
-
 # Create the ggplot
 ggplot(nowcast_sample_mse, aes(x = doq, group = sample)) +
   # Add the first time series
@@ -390,128 +358,17 @@ ggplot(nowcast_sample_mse, aes(x = doq, group = sample)) +
   theme_minimal()+
   # Customize the colors and legend
   scale_color_manual(name = 'OLS Model', values = c('pmi' = 'blue', 'news' = 'red'))
-  # Move the legend to the bottom
-  # Move the legend inside the plot frame at the top right
-  #theme(legend.position.inside = c(1, 1), legend.justification = c(1, 1))
 
 
-# ggplot(nowcast_sample_mse, aes(x = doq, y = mse, color = model)) +
-#   geom_line() +
-#   facet_wrap(~ sample, ncol = 2) +
-#   labs(title = "Mean Squared Error of Forecast Over Time",
-#        x = "Time",
-#        y = "Mean Squared Error",
-#        color = "Model") +
-#   theme_minimal()
-
-
-
+## save plot
 ggsave(filename = paste0(results_path, "ols_mse.png"),
        bg = "white")
-?ggsave
+
 ### save results
 
 write.csv(nowcast_results_dt, file = paste0(results_path, "nowcast_results_ols.csv"),
           row.names = FALSE)
 
-### diagnostics
-
-# nowcast_results_dt[se_pmi_bench > se_news,]
-# 
-# nowcast_results_dt[se_news > se_pmi_bench,]
-# 
-# max(na.omit(nowcast_results_dt[, mse_news]))
-# 
-# nowcast_results_dt[, mse_news]
-# 
-# mse_max <- which(nowcast_results_dt[, mse_news] %in% max(na.omit(nowcast_results_dt[, mse_news])))
-# 
-# nowcast_results_dt[mse_max,]
-# 
-# nowcast_results_dt[mse_news %in% 0.5321892, ]
-# 
-# test_dt <- nowcast_results_dt[doq %in% 33 , ]
-# 
-# daily_data[day %in% "2001-05-01", ]
-# daily_data[]
-# 
-# news_diff
-
-# single nowcast
-
-# curr_day <- as.character(daily_data[1035, day]) # 1035 = 2000-01-01
-# 
-# curr_doq <- daily_data[1035, doq]
-# 
-# curr_pmi_release <- daily_data[1035, pmi_available]
-# 
-# 
-# current_nowcast_dt <- daily_data[1:1035]
-# 
-# current_nowcast_dt <- current_nowcast_dt[doq %in% curr_doq,] # filter for obs with same doq
-# current_nowcast_dt <- current_nowcast_dt[pmi_available %in% curr_pmi_release,] # filter for obs with same pmi release cycle stage
-# 
-# curr_gdp_vintage <- real_time_gdp[[curr_day]]
-# 
-# curr_gdp_vintage[, gdp_vintage_gr:= na_diff(log(value))]
-# 
-# 
-# curr_day
-# 
-# real_time_gdp[[curr_day]]
-# 
-# nowcast_dt <- current_nowcast_dt[curr_gdp_vintage, , on = .(quarter = quarter)]
-# 
-# nowcast_dt <- curr_gdp_vintage[current_nowcast_dt, , on = .(quarter = quarter)]
-# nowcast_dt <- nowcast_dt[,.(day, quarter, doq, pmi, pmi_available,news, gdp_vintage_gr, gdp_gr)]
-# 
-# # split into in and out of sample
-# 
-# nowcast_in_dt <- na.omit(nowcast_dt)
-# 
-# nowcast_out_dt <- nowcast_dt[which(is.na(nowcast_dt[,.(gdp_vintage_gr)])),] # filters obs where no gdp data was available
-# 
-# # nowcast model estimation
-# 
-# nowcast_model_pmi_bench <- lm(gdp_vintage_gr ~ pmi, nowcast_in_dt) # pmi benchmark
-# summary(nowcast_model_pmi_bench)
-# 
-# nowcast_model_news <- lm(gdp_vintage_gr ~ pmi + news, nowcast_in_dt) # news 
-# summary(nowcast_model_news)
-# 
-# # prediction
-# nowcast_pmi_bench_pred <- predict(nowcast_model_pmi_bench, nowcast_out_dt)
-# 
-# nowcast_news_pred <- predict(nowcast_model_news, nowcast_out_dt)
-# 
-# # SE
-# 
-# nowcast_pmi_bench_se <- (nowcast_out_dt$gdp_gr - nowcast_pmi_bench_pred)^2
-# 
-# nowcast_news_se <- (nowcast_out_dt$gdp_gr - nowcast_news_pred)^2
-# 
-# # MSE
-# 
-# nowcast_pmi_bench_mse <- mean((nowcast_out_dt$gdp_gr - nowcast_pmi_bench_pred)^2)
-# 
-# nowcast_news_mse <- mean((nowcast_out_dt$gdp_gr - nowcast_news_pred)^2)
-# 
-# # summary dt for nowcast
-# 
-# nowcast_results <- data.table(
-#   day = curr_day,
-#   quarter = nowcast_out_dt[, quarter],
-#   doq = curr_doq,
-#   gdp_gr = nowcast_out_dt[, gdp_gr],
-#   prediction_pmi_bench = nowcast_pmi_bench_pred,
-#   prediction_news = nowcast_news_pred,
-#   se_pmi_bench = nowcast_pmi_bench_se,
-#   se_news = nowcast_news_se,
-# )
-# 
-# nowcast_results[, nowcast_number:= 1:.N]
-# 
-# 
 
 # DFM ------------------------------------------------------------
 
@@ -548,7 +405,7 @@ nowcast_results_mac <- vector("list", length = length(nowcast_start:nowcast_end)
 
 # # test
 # 
-# i <- 6000
+# i <- 3607
 # k <- 45
 # nowcast_results_mac[[i]]
 ## loop start
@@ -558,7 +415,7 @@ for(i in nowcast_start:nowcast_end){
   k <- (i-nowcast_start + 1)
   print(k) # for observing progress
   
-  curr_day <- as.character(daily_data[i, day]) # i_start = 2000-01-01
+  curr_day <- as.character(daily_data[i, day]) 
   
   curr_doq <- daily_data[i, doq]
   curr_dom <- daily_data[i, dom]
@@ -716,49 +573,7 @@ for(i in nowcast_start:nowcast_end){
   
   # nowcast.plot(now_bench, type = "eigenvectors")
   # nowcast.plot(now_news, type = "eigenvectors")
-  # 
-  # ## split into in and out of sample
-  # 
-  # nowcast_in_index <- which(is.na(nowcast_dt$gdp_vintage_gr))
-  # nowcast_in_dt <- nowcast_dt[!(nowcast_in_index),]
-  # 
-  # current_in_obs <- nrow(nowcast_in_dt)
-  # 
-  # # fixed or expanding window
-  # 
-  # if(window == "fixed"){
-  #   
-  #   if(k == 1){
-  #     fixed_window_obs <- nrow(nowcast_in_dt)
-  #   } else{
-  #     nowcast_in_dt <- nowcast_in_dt[(current_in_obs-fixed_window_obs+1):current_in_obs,]
-  #     
-  #   }
-  #   
-  # }
-  # 
-  # 
-  # if(nrow(nowcast_in_dt) > 0){
-  #   
-  #   nowcast_out_dt <- nowcast_dt[nowcast_in_index,] # filters obs where no gdp data was available
-  #   
-  #   # nowcast model estimation
-  #   
-  #   nowcast_model_pmi_bench <- lm(gdp_vintage_gr ~ pmi, nowcast_in_dt) # pmi benchmark
-  #   summary(nowcast_model_pmi_bench)
-  #   
-  #   nowcast_model_news <- lm(gdp_vintage_gr ~ pmi + news_aoq, nowcast_in_dt) # news aoq
-  #   summary(nowcast_model_news)
-  #   
-  #   nowcast_model_news_cci <- lm(gdp_vintage_gr ~ pmi + news_aoq + cci, nowcast_in_dt) # news aoq + cci
-  #   
-  #   # prediction
-  #   nowcast_pmi_bench_pred <- predict(nowcast_model_pmi_bench, nowcast_out_dt)
-  #   
-  #   nowcast_news_pred <- predict(nowcast_model_news, nowcast_out_dt)
-  #   
-  #   nowcast_news_cci_pred <- predict(nowcast_model_news_cci, nowcast_out_dt)
-    
+  
     # SE
   
     curr_gdp_gr <- daily_data[i, gdp_gr]
@@ -790,23 +605,6 @@ for(i in nowcast_start:nowcast_end){
       #se_news_cci = nowcast_news_cci_se
     )
     
-    # nowcast_results_mac[[k]][, nowcast_number:= 1]
-    
-    
-  # } else{
-  #   nowcast_results_mac[[k]] <- data.table(
-  #     day = curr_day,
-  #     quarter = nowcast_out_dt[, quarter],
-  #     doq = curr_doq,
-  #     gdp_gr = nowcast_out_dt[, gdp_gr],
-  #     prediction_pmi_bench = NA,
-  #     prediction_news = NA,
-  #     prediction_news_cci = NA,
-  #     se_pmi_bench = NA,
-  #     se_news = NA,
-  #     se_news_cci = NA,
-  #     nowcast_number= NA
-  #   )
 }
   
   
@@ -943,19 +741,6 @@ ggplot(nowcast_dfm_sample_mse_new, aes(x = doq, group = sample)) +
   theme_minimal()+
   # Customize the colors and legend
   scale_color_manual(name = 'DFM Model', values = c('bench' = 'blue', 'news' = 'red'))
-# Move the legend to the bottom
-# Move the legend inside the plot frame at the top right
-#theme(legend.position.inside = c(1, 1), legend.justification = c(1, 1))
-
-
-# ggplot(nowcast_sample_mse, aes(x = doq, y = mse, color = model)) +
-#   geom_line() +
-#   facet_wrap(~ sample, ncol = 2) +
-#   labs(title = "Mean Squared Error of Forecast Over Time",
-#        x = "Time",
-#        y = "Mean Squared Error",
-#        color = "Model") +
-#   theme_minimal()
 
 
 # save plot
